4d0c8f9df3bd47df951c9a1564b264edbfabf97b
Emil Kristensen <emil00y1@stud.kea.dk>
2023-02-17 10:09:31 +0100

    værsgo

--- js/produktoversigt.js
+++ js/produktoversigt.js
@@ -1,12 +1,24 @@
-fetch("https://drinkssite-a008.restdb.io/rest/drinks", {
-  method: "get",
-  headers: {
-    "x-apikey": "63e9f4e4478852088da6810f",
-  },
-})
+fetch(
+  "https://drinkssite-a008.restdb.io/rest/drinks?q={%22category%22:%20%22sour%22}",
+  {
+    method: "get",
+    headers: {
+      "x-apikey": "63e9f4e4478852088da6810f",
+    },
+  }
+)
   .then((e) => e.json())
-  .then(addCategory);
+  .then(showDrinks);
 
-function addCategory(data) {
-  console.log(data);
+function showDrinks(drinks) {
+  console.log(drinks);
+  drinks.forEach(showDrinks);
+}
+
+function showDrinks(drink) {
+  const template = document.querySelector("#drinks_template").content;
+
+  const copy = template.cloneNode(true);
+
+  document.querySelector(".content").appendChild(copy);
 }
