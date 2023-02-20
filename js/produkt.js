cced986cde9fae664b8a77830b93d32073061cfb
Ida Svale <idasvale@gmail.com>
2023-02-20 13:51:31 +0100

    svg

---
+++ js/produkt.js
@@ -0,0 +1,38 @@
+fetch("https://drinkssite-a008.restdb.io/rest/drinks?q={%22category%22:%20%22sour%22}", {
+  method: "get",
+  headers: {
+    "x-apikey": "63e9f4e4478852088da6810f",
+  },
+})
+  .then((e) => e.json())
+  .then(showDrinks);
+
+function showDrinks(drinks) {
+  console.log("drinks");
+  drinks.forEach(showDrink);
+}
+
+function showDrink(drink) {
+  const template = document.querySelector("#drinks_template").content;
+  const copy = template.cloneNode(true);
+
+  copy.querySelector("h2").textContent = drink.name;
+
+  /*  const tags = drink.tags;
+    const comma = ",";
+    const splitTags = tags.split(comma);
+  
+  
+    const tagContainer = copy.querySelector(".tag_container");
+  
+    splitTags.forEach(createTag); */
+
+  document.querySelector(".content").appendChild(copy);
+
+  /* function createTag(tag) {
+      const tagTemplate = document.querySelector("#tag_template").content;
+      const tagCopy = tagTemplate.cloneNode(true);
+      tagCopy.querySelector("p").textContent = "#" + tag;
+      tagContainer.appendChild(tagCopy);
+    } */
+}
