edd655756404bfd04862f2a03f8088800b5c99e7
Emil Kristensen <emil00y1@stud.kea.dk>
2023-02-21 21:33:44 +0100

    rettet småting + dynamisk produktsite

--- js/produkt.js
+++ js/produkt.js
@@ -1,14 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const drinkname = urlParams.get("drinkname");
 
-<<<<<<< HEAD
-fetch("https://drinkssite-a008.restdb.io/rest/drinks?q={%22name%22:%20%22" + drinkname + "%22}", {
-  method: "get",
-  headers: {
-    "x-apikey": "63e9f4e4478852088da6810f",
-  },
-})
-=======
 fetch(
   "https://drinkssite-a008.restdb.io/rest/drinks?q={%22name%22:%20%22" +
     drinkname +
@@ -20,27 +12,36 @@ fetch(
     },
   }
 )
->>>>>>> produkt-v-4
   .then((e) => e.json())
   .then(showContent);
 
 function showContent(product) {
   console.log(product);
-  document.querySelector("h1").textContent = product.name;
+  document.querySelector("h4").textContent = product[0].name;
+  document.querySelector(".recipe_content").textContent = product[0].recipe;
+  document.querySelector(".recipe_content").textContent = product[0].recipe;
+
+  const imagePath = "img/" + drinkname + ".svg";
+  const imagePath2 = imagePath.replace("ñ", "n");
+  const imagePathNoSpace = imagePath2.replace(/ +/g, "_");
+
+  document.querySelector(".drink img").src = imagePathNoSpace;
+
+  const ingredients = product[0].ingredients;
+  const comma = ",";
+  const ingredient = ingredients.split(comma);
+
+  ingredient.forEach(createIngredient);
+
+  document.querySelector("title").textContent = drinkname;
 }
 
-/*  const tags = drink.tags;
-    const comma = ",";
-    const splitTags = tags.split(comma);
-  
-  
-    const tagContainer = copy.querySelector(".tag_container");
-  
-    splitTags.forEach(createTag); */
+function createIngredient(ingredient) {
+  const ingredientTemplate = document.querySelector(
+    "#ingredientTemplate"
+  ).content;
+  const ingredientCopy = ingredientTemplate.cloneNode(true);
+  ingredientCopy.querySelector("p").textContent = "- " + ingredient;
 
-/* function createTag(tag) {
-      const tagTemplate = document.querySelector("#tag_template").content;
-      const tagCopy = tagTemplate.cloneNode(true);
-      tagCopy.querySelector("p").textContent = "#" + tag;
-      tagContainer.appendChild(tagCopy);
-    } */
+  document.querySelector(".ingredients_content").appendChild(ingredientCopy);
+}
